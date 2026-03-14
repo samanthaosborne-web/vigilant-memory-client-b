d907bdf270fefa7f11bcec6cf43dd42e5ea2f138
samanthaosborne-web <samanthaosborne@hotmail.com>
2026-03-15 06:51:05 +0800

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -4,4 +4,4 @@
 window.SUPABASE_URL = "https://afuxydpqhiwxbgtlrxyv.supabase.co";
 // Use only the publishable key in browser code.
 // Do NOT put your secret key in frontend files.
-window.SUPABASE_ANON_KEY = "sb_publishable_0BXYsU7nGVmcQA883DUUJA_zgsFvUHI";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmdXh5ZHBxaGl3eGJndGxyeHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODMzNzQsImV4cCI6MjA4ODU1OTM3NH0.mWp0Iqs-9fhYU-qH8DMF28qRUj2ouPZN55FmEPirfVs";
